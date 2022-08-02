c51d73fc0d03ef819f4cbb3ce5c1220e41098d2e
Michael Wang <michaelwangxv@gmail.com>
2022-08-02 19:47:48 +0000

    Add typewriter

---
+++ typewriter.js
@@ -0,0 +1,20 @@
+// Sentence to type
+let sentence = "hello there from lighthouse labs";
+
+// Add a newline character to the end
+sentence += "\n";
+
+// Set the time delay
+let time = 250;
+
+// Iterate through the characters of sentence
+for (const char of sentence) {
+
+  // Print each character 250ms after the other
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time)
+
+  // Increment the time by 250ms
+  time += 250;
+}
